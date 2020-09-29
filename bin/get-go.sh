7a5f05f35a513502ccbf40bd826fa297d9429cc8
Kingdon Barrett <kingdon.b@nd.edu>
2020-09-29 11:58:40 -0400

    bump version to latest golang - 1.15.2

--- bin/get-go.sh
+++ bin/get-go.sh
@@ -28,7 +28,7 @@ mkdir -p ${GOPATH}/src ${GOPATH}/bin
 #CMD ["make"]
 # source: https://stackoverflow.com/a/53405005/661659
 
-version=1.15.1
+version=1.15.2
 echo "Installing go $version (with CGO_ENABLED=0)"
 
 set -x
